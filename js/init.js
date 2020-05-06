7d216f25d34117543290389728c3dd2410bfa5cb
Risyandi <Risyandi.94@gmail.com>
2020-05-06 20:33:26 +0700

    formatting

--- js/init.js
+++ js/init.js
@@ -2,17 +2,17 @@
  * Copyright (c) 2017 Marketify
  * Author: Marketify
  * This file is made for CURRENT TEMPLATE
-*/
+ */
 
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
 
 	"use strict";
-	
-	
-	
+
+
+
 	// here all ready functions
-	
+
 	edina_tm_hamburger();
 	edina_tm_imgtosvg();
 	edina_tm_magnific_popup();
@@ -31,13 +31,13 @@ jQuery(document).ready(function(){
 	// edina_tm_popup_blog();
 	clickSend();
 
-	
-	jQuery(window).on('scroll',function(){
+
+	jQuery(window).on('scroll', function () {
 		//e.preventDefault();
 		edina_tm_totop_myhide();
 		edina_tm_nav_bg_scroll();
 	});
-	
+
 });
 
 // -----------------------------------------------------
@@ -48,23 +48,23 @@ jQuery(document).ready(function(){
 // ---------------    IMAGE TO SVG    ------------------
 // -----------------------------------------------------
 
-function edina_tm_imgtosvg(){
-	
-	"use strict";
-	
-	jQuery('img.svg').each(function(){
-		
-		var jQueryimg 		= jQuery(this);
-		var imgClass		= jQueryimg.attr('class');
-		var imgURL			= jQueryimg.attr('src');
+function edina_tm_imgtosvg() {
 
-		jQuery.get(imgURL, function(data) {
+	"use strict";
+
+	jQuery('img.svg').each(function () {
+
+		var jQueryimg = jQuery(this);
+		var imgClass = jQueryimg.attr('class');
+		var imgURL = jQueryimg.attr('src');
+
+		jQuery.get(imgURL, function (data) {
 			// Get the SVG tag, ignore the rest
 			var jQuerysvg = jQuery(data).find('svg');
 
 			// Add replaced image's classes to the new SVG
-			if(typeof imgClass !== 'undefined') {
-				jQuerysvg = jQuerysvg.attr('class', imgClass+' replaced-svg');
+			if (typeof imgClass !== 'undefined') {
+				jQuerysvg = jQuerysvg.attr('class', imgClass + ' replaced-svg');
 			}
 
 			// Remove any invalid XML tags as per http://validator.w3.org
@@ -82,20 +82,20 @@ function edina_tm_imgtosvg(){
 // ---------------  HAMBURGER  -------------------------
 // -----------------------------------------------------
 
-function edina_tm_hamburger(){
-	
+function edina_tm_hamburger() {
+
 	"use strict";
-	
-	var hamburger 		= jQuery('.hamburger');
-	var mobileMenu		= jQuery('.edina_tm_mobile_menu_wrap');
-	
-	hamburger.on('click',function(){
-		var element 	= jQuery(this);
-		
-		if(element.hasClass('is-active')){
+
+	var hamburger = jQuery('.hamburger');
+	var mobileMenu = jQuery('.edina_tm_mobile_menu_wrap');
+
+	hamburger.on('click', function () {
+		var element = jQuery(this);
+
+		if (element.hasClass('is-active')) {
 			element.removeClass('is-active');
 			mobileMenu.slideUp();
-		}else{
+		} else {
 			element.addClass('is-active');
 			mobileMenu.slideDown();
 		}
@@ -107,37 +107,37 @@ function edina_tm_hamburger(){
 // --------------    MAGNIFIC POPUP    -----------------
 // -----------------------------------------------------
 
-function edina_tm_magnific_popup(){
-	
+function edina_tm_magnific_popup() {
+
 	"use strict";
-	
+
 	jQuery('.open-popup-link').magnificPopup({
-		type:'inline',
+		type: 'inline',
 		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 	});
-	
-	jQuery('.gallery').each(function() { // the containers for all your galleries
+
+	jQuery('.gallery').each(function () { // the containers for all your galleries
 		jQuery(this).magnificPopup({
 			delegate: 'a', // the selector for gallery item
 			type: 'image',
 			gallery: {
-			  enabled:true
+				enabled: true
 			}
 		});
 	});
-	jQuery('.gallery_zoom').each(function() { // the containers for all your galleries
+	jQuery('.gallery_zoom').each(function () { // the containers for all your galleries
 		jQuery(this).magnificPopup({
 			delegate: 'a.zoom', // the selector for gallery item
 			type: 'image',
 			gallery: {
-			  enabled:true
+				enabled: true
 			},
 			removalDelay: 300,
 			mainClass: 'mfp-fade'
 		});
-		
+
 	});
-	jQuery('.popup-youtube').each(function() { // the containers for all your galleries
+	jQuery('.popup-youtube').each(function () { // the containers for all your galleries
 		jQuery(this).magnificPopup({
 			//type: 'iframe',
 			disableOn: 700,
@@ -154,20 +154,20 @@ function edina_tm_magnific_popup(){
 // --------------------    JARALLAX    -----------------
 // -----------------------------------------------------
 
-function edina_tm_jarallax(){
-	
+function edina_tm_jarallax() {
+
 	"use strict";
-	
-	jQuery('.jarallax').each(function(){
-		var element			= jQuery(this);
-		var	customSpeed		= element.data('speed');
-		
-		if(customSpeed !== "undefined" && customSpeed !== ""){
+
+	jQuery('.jarallax').each(function () {
+		var element = jQuery(this);
+		var customSpeed = element.data('speed');
+
+		if (customSpeed !== "undefined" && customSpeed !== "") {
 			customSpeed = customSpeed;
-		}else{
-			customSpeed 	= 0.5;
+		} else {
+			customSpeed = 0.5;
 		}
-		
+
 		element.jarallax({
 			speed: customSpeed
 		});
@@ -180,64 +180,72 @@ function edina_tm_jarallax(){
 
 // filterable 
 
-function edina_tm_portfolio(){
+function edina_tm_portfolio() {
 
 	"use strict";
 
-	if(jQuery().isotope) {
+	if (jQuery().isotope) {
 
 		// Needed variables
-		var list 		 = jQuery('.edina_tm_portfolio_list');
-		var filter		 = jQuery('.edina_tm_portfolio_filter');
+		var list = jQuery('.edina_tm_portfolio_list');
+		var filter = jQuery('.edina_tm_portfolio_filter');
 
-		if(filter.length){
+		if (filter.length) {
 			// Isotope Filter 
-			filter.find('a').on('click', function(){
+			filter.find('a').on('click', function () {
 				var selector = jQuery(this).attr('data-filter');
-				list.isotope({ 
-					filter				: selector,
-					animationOptions	: {
-						duration			: 750,
-						easing				: 'linear',
-						queue				: false
+				list.isotope({
+					filter: selector,
+					animationOptions: {
+						duration: 750,
+						easing: 'linear',
+						queue: false
 					}
 				});
 				return false;
-			});	
+			});
 
 			// Change active element class
-			filter.find('a').on('click', function() {
+			filter.find('a').on('click', function () {
 				filter.find('a').removeClass('current');
 				jQuery(this).addClass('current');
 				return false;
-			});	
+			});
 		}
 	}
 }
 
-function edina_tm_portfolio_animation(){
-	
+function edina_tm_portfolio_animation() {
+
 	"use strict";
-	
-	var list			= jQuery('.edina_tm_portfolio_list > li');
-	
-	list.each(function(){
-		var el			= jQuery(this);
-		var el2			= el.find('.list_inner');
-		var image		= el.find('.image_wrap');
-		var definition	= el.find('.definition_portfolio');
-		var definitionH	= el.find('.definition_portfolio').outerHeight();
-		
-		el2.each(function(){
-			var el3			= jQuery(this);
-			el3.on('mouseenter',function(){
-				image.css({top:-definitionH/2});
-				definition.css({marginTop:-definitionH});
-			}).on('mouseleave',function(){
-				image.css({top:0});
-				definition.css({marginTop:0});
+
+	var list = jQuery('.edina_tm_portfolio_list > li');
+
+	list.each(function () {
+		var el = jQuery(this);
+		var el2 = el.find('.list_inner');
+		var image = el.find('.image_wrap');
+		var definition = el.find('.definition_portfolio');
+		var definitionH = el.find('.definition_portfolio').outerHeight();
+
+		el2.each(function () {
+			var el3 = jQuery(this);
+			el3.on('mouseenter', function () {
+				image.css({
+					top: -definitionH / 2
+				});
+				definition.css({
+					marginTop: -definitionH
+				});
+			}).on('mouseleave', function () {
+				image.css({
+					top: 0
+				});
+				definition.css({
+					marginTop: 0
+				});
 			});
-			
+
 		});
 	});
 }
@@ -246,28 +254,30 @@ function edina_tm_portfolio_animation(){
 // --------------------    TOTOP    --------------------
 // -----------------------------------------------------
 
-function edina_tm_totop(){
-	
+function edina_tm_totop() {
+
 	"use strict";
-	
-	jQuery(".edina_tm_to_top_wrap").on('click', function(e) {
-		e.preventDefault();		
-		jQuery("html, body").animate({ scrollTop: 0 }, 'slow');
+
+	jQuery(".edina_tm_to_top_wrap").on('click', function (e) {
+		e.preventDefault();
+		jQuery("html, body").animate({
+			scrollTop: 0
+		}, 'slow');
 		return false;
 	});
 }
 
-function edina_tm_totop_myhide(){
-	
+function edina_tm_totop_myhide() {
+
 	"use strict";
-	
-	var toTop			=jQuery(".edina_tm_to_top_wrap");
-	if(toTop.length){
-		var topOffSet 	=toTop.offset().top;
-		
-		if(topOffSet > 1350){
-			toTop.addClass('opened');	
-		}else{
+
+	var toTop = jQuery(".edina_tm_to_top_wrap");
+	if (toTop.length) {
+		var topOffSet = toTop.offset().top;
+
+		if (topOffSet > 1350) {
+			toTop.addClass('opened');
+		} else {
 			toTop.removeClass('opened');
 		}
 	}
@@ -277,48 +287,49 @@ function edina_tm_totop_myhide(){
 // ------------    NAV BACKGROUND  SCROLL    -----------
 // -----------------------------------------------------
 
-function edina_tm_nav_bg_scroll(){
-	
+function edina_tm_nav_bg_scroll() {
+
 	"use strict";
-	
-	var header 			= jQuery('.edina_tm_header');
-	var windowScroll	= jQuery(window).scrollTop();
-	var W				= jQuery(window).width();
-	
-	if(W>1040){
-		jQuery(window).scroll(function(){
-            if(windowScroll >= '100'){
-                header.addClass('scroll');
-            }
-            else{
-                header.removeClass('scroll');  
-            }
-        });
-	} 
+
+	var header = jQuery('.edina_tm_header');
+	var windowScroll = jQuery(window).scrollTop();
+	var W = jQuery(window).width();
+
+	if (W > 1040) {
+		jQuery(window).scroll(function () {
+			if (windowScroll >= '100') {
+				header.addClass('scroll');
+			} else {
+				header.removeClass('scroll');
+			}
+		});
+	}
 }
 
 // -----------------------------------------------------
 // ------------    ANCHOR NAVIGATION    ----------------
 // -----------------------------------------------------
 
-function edina_tm_anchor(){
-	
+function edina_tm_anchor() {
+
 	"use strict";
-	
+
 	jQuery('.anchor_nav').onePageNav();
-	
+
 	var scrollOffset = 0;
-	
-	jQuery(".anchor a").on('click', function(evn){
+
+	jQuery(".anchor a").on('click', function (evn) {
 		evn.preventDefault();
 		jQuery('html,body').scrollTo(this.hash, this.hash, {
-			gap: { y: -scrollOffset-85 },
-			animation:{
+			gap: {
+				y: -scrollOffset - 85
+			},
+			animation: {
 				duration: 1500,
 				easing: "easeInOutExpo"
 			}
 		});
-		return false;	
+		return false;
 	});
 }
 
@@ -326,45 +337,49 @@ function edina_tm_anchor(){
 // ----------------    CONTACT FORM    -----------------
 // -----------------------------------------------------
 
-function edina_tm_contact_form(){
-	
+function edina_tm_contact_form() {
+
 	"use strict";
-	
-	jQuery(".contact_form #send_message").on('click', function(){
-		
-		var name 		= jQuery(".contact_form #name").val();
-		var email 		= jQuery(".contact_form #email").val();
-		var message 	= jQuery(".contact_form #message").val();
-		var subject 	= jQuery(".contact_form #subject").val();
-		var success     = jQuery(".contact_form .returnmessage").data('success');
-	
+
+	jQuery(".contact_form #send_message").on('click', function () {
+
+		var name = jQuery(".contact_form #name").val();
+		var email = jQuery(".contact_form #email").val();
+		var message = jQuery(".contact_form #message").val();
+		var subject = jQuery(".contact_form #subject").val();
+		var success = jQuery(".contact_form .returnmessage").data('success');
+
 		jQuery(".contact_form .returnmessage").empty(); //To empty previous error/success message.
 		//checking for blank fields	
-		if(name===''||email===''||message===''){
-			
+		if (name === '' || email === '' || message === '') {
+
 			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
-		}
-		else{
+		} else {
 			// Returns successful data submission message when the entered information is stored in database.
-			jQuery.post("modal/contact.php",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
-				
-				jQuery(".contact_form .returnmessage").append(data);//Append returned message to message paragraph
-				
-				
-				if(jQuery(".contact_form .returnmessage span.contact_error").length){
-					jQuery(".contact_form .returnmessage").slideDown(500).delay(2000).slideUp(500);		
-				}else{
-					jQuery(".contact_form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
+			jQuery.post("modal/contact.php", {
+				ajax_name: name,
+				ajax_email: email,
+				ajax_message: message,
+				ajax_subject: subject
+			}, function (data) {
+
+				jQuery(".contact_form .returnmessage").append(data); //Append returned message to message paragraph
+
+
+				if (jQuery(".contact_form .returnmessage span.contact_error").length) {
+					jQuery(".contact_form .returnmessage").slideDown(500).delay(2000).slideUp(500);
+				} else {
+					jQuery(".contact_form .returnmessage").append("<span class='contact_success'>" + success + "</span>");
 					jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
 				}
-				
-				if(data===""){
-					jQuery("#contact_form")[0].reset();//To reset form fields on success
+
+				if (data === "") {
+					jQuery("#contact_form")[0].reset(); //To reset form fields on success
 				}
-				
+
 			});
 		}
-		return false; 
+		return false;
 	});
 }
 
@@ -372,45 +387,69 @@ function edina_tm_contact_form(){
 // --------------------    OWL CAROUSEL    -------------
 // -----------------------------------------------------
 
-function edina_tm_owl_carousel(){
-	
+function edina_tm_owl_carousel() {
+
 	"use strict";
-	
-	var carusel2			= jQuery('.edina_tm_testimonial_wrap .owl-carousel');
-  	carusel2.owlCarousel({
-		loop:true,
-		margin:70,
-		autoplay:7000,
+
+	var carusel2 = jQuery('.edina_tm_testimonial_wrap .owl-carousel');
+	carusel2.owlCarousel({
+		loop: true,
+		margin: 70,
+		autoplay: 7000,
 		autoWidth: false,
 		nav: false,
-		items:3,
-		smartSpeed:5000,
-		responsive:{
-			0:{items:1},
-			480:{items:2},
-			768:{items:3},
-			1040:{items:3},
-			1600:{items:3},
-			1920:{items:3}
+		items: 3,
+		smartSpeed: 5000,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			1040: {
+				items: 3
+			},
+			1600: {
+				items: 3
+			},
+			1920: {
+				items: 3
+			}
 		}
 	});
-	
-	var carusel3			= jQuery('.edina_tm_partners_wrap .owl-carousel');
-  	carusel3.owlCarousel({
-		loop:true,
-		margin:40,
-		autoplay:6000,
+
+	var carusel3 = jQuery('.edina_tm_partners_wrap .owl-carousel');
+	carusel3.owlCarousel({
+		loop: true,
+		margin: 40,
+		autoplay: 6000,
 		autoWidth: false,
 		nav: false,
-		items:4,
-		smartSpeed:3000,
-		responsive:{
-			0:{items:1},
-			480:{items:2},
-			768:{items:3},
-			1040:{items:4},
-			1600:{items:4},
-			1920:{items:4}
+		items: 4,
+		smartSpeed: 3000,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			1040: {
+				items: 4
+			},
+			1600: {
+				items: 4
+			},
+			1920: {
+				items: 4
+			}
 		}
 	});
 }
@@ -419,26 +458,29 @@ function edina_tm_owl_carousel(){
 // --------------------    WOW JS    -------------------
 // -----------------------------------------------------
 
- new WOW().init();
+new WOW().init();
 
 // -----------------------------------------------------
 // ---------------    HERO TEXT ANIATION  --------------
 // -----------------------------------------------------
 
-function edina_tm_text_animation(){
-	
+function edina_tm_text_animation() {
+
 	"use strict";
-	
-	var H        			= jQuery(window).height();
-	var titleHolder			= jQuery('.edina_tm_universal_box_wrap .hero_title');
-	var titleHeight			= titleHolder.outerHeight();
-	var headerHeight		= jQuery('.edina_tm_header').outerHeight();
-	
-	var	height				= H/2 + titleHeight/2 - headerHeight;
-	
-	jQuery(window).on('scroll',function(){
+
+	var H = jQuery(window).height();
+	var titleHolder = jQuery('.edina_tm_universal_box_wrap .hero_title');
+	var titleHeight = titleHolder.outerHeight();
+	var headerHeight = jQuery('.edina_tm_header').outerHeight();
+
+	var height = H / 2 + titleHeight / 2 - headerHeight;
+
+	jQuery(window).on('scroll', function () {
 		var window_offset = jQuery(window).scrollTop();
-		titleHolder.css({opacity:1 - (window_offset/height), marginTop:(window_offset/height)*200});
+		titleHolder.css({
+			opacity: 1 - (window_offset / height),
+			marginTop: (window_offset / height) * 200
+		});
 	});
 }
 
@@ -446,18 +488,18 @@ function edina_tm_text_animation(){
 // -------------   ANIMATE TEXT  -------------------
 // -------------------------------------------------
 
-function edina_tm_animate_text(){
-	
+function edina_tm_animate_text() {
+
 	"use strict";
-	
-	var animateSpan			= jQuery('.edina_tm_animation_text_word');
-	
-		animateSpan.typed({
-			strings: ["GRAPHIC DESIGNER", "PHOTOGRAPHER", "WEB DEVELOPER"],
-			loop: true,
-			startDelay: 1e3,
-			backDelay: 2e3
-		});
+
+	var animateSpan = jQuery('.edina_tm_animation_text_word');
+
+	animateSpan.typed({
+		strings: ["GRAPHIC DESIGNER", "PHOTOGRAPHER", "WEB DEVELOPER"],
+		loop: true,
+		startDelay: 1e3,
+		backDelay: 2e3
+	});
 }
 
 // -----------------------------------------------------
@@ -470,7 +512,7 @@ function edina_tm_animate_text(){
 // 	var popupBox		= jQuery('#edina_tm_popup_blog');
 // 	var popupInner		= popupBox.find('.inner_popup');
 // 	var closePopup		= popupBox.find('.close');
-	
+
 // 	li.each(function(){
 // 		var element		= jQuery(this);
 // 		var button		= element.find('.read_more a,.title_holder a,.link_news');
@@ -479,7 +521,7 @@ function edina_tm_animate_text(){
 // 		var imgData		= mainImage.data('url');
 // 		var title		= element.find('.title_holder h3');
 // 		var titleHref	= element.find('.title_holder h3 a').html();
-		
+
 // 		mainImage.css({backgroundImage: 'url('+imgData+')'});
 // 		button.on('click',function(){
 // 			popupBox.addClass('opened');
@@ -502,32 +544,36 @@ function edina_tm_animate_text(){
 // -------------    BLOG MENU SCROLL -------------------
 // -----------------------------------------------------
 
-function edina_tm_popupscroll(){
-	
+function edina_tm_popupscroll() {
+
 	"use strict";
-	
-	var H				= jQuery(window).height();
-	var scrollable		= jQuery('.scrollable');
-	
-	var popupBox		= jQuery('.edina_tm_popup_blog .inner_popup');
-	
-	popupBox.css({height:H-100});
-	
-	scrollable.each(function(){
-		var element		= jQuery(this);
-		var wH			= jQuery(window).height();
-		
-		element.css({height: wH-100});
-		
+
+	var H = jQuery(window).height();
+	var scrollable = jQuery('.scrollable');
+
+	var popupBox = jQuery('.edina_tm_popup_blog .inner_popup');
+
+	popupBox.css({
+		height: H - 100
+	});
+
+	scrollable.each(function () {
+		var element = jQuery(this);
+		var wH = jQuery(window).height();
+
+		element.css({
+			height: wH - 100
+		});
+
 		element.niceScroll({
-			touchbehavior:false,
-			cursorwidth:0,
-			autohidemode:true,
-			cursorborder:"0px solid #fff"
+			touchbehavior: false,
+			cursorwidth: 0,
+			autohidemode: true,
+			cursorborder: "0px solid #fff"
 		});
 	});
 }
- $(".youtube-bg").mb_YTPlayer();
+$(".youtube-bg").mb_YTPlayer();
 
 
 //  send message
