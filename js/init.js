181732b98a05f4bb186c51c30c16c2701c851b8e
Risyandi <Risyandi.94@gmail.com>
2021-02-10 00:18:54 +0700

    update running text

--- js/init.js
+++ js/init.js
@@ -496,7 +496,7 @@ function edina_tm_animate_text() {
 	var animateSpan = jQuery('.edina_tm_animation_text_word');
 
 	animateSpan.typed({
-		strings: ["GRAPHIC DESIGNER", "PHOTOGRAPHER", "WEB DEVELOPER"],
+		strings: ["GRAPHIC DESIGNER", "PHOTOGRAPHER", "SOFTWARE DEVELOPMENT ENGINEER", "CYCLIST"],
 		loop: true,
 		startDelay: 1e3,
 		backDelay: 2e3
